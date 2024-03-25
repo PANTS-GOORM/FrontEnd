4c252ff777f93f60d4cdc8c99ee07bb081fc7bc2
김민범 <beom0109@naver.com>
2024-03-25 22:09:18 +0900

    💚 Eslint 오류 수정
    eslint 오류 수정

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useStore from "../../store/learning"; // 상태를 가져오기 위한 useStore 훅을 임포트합니다.
 import TypeModalButton from "../../components/Button/TypeModalButton";
 
 function Main() {
