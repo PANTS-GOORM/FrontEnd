0a36f189fe319ddb6fd748fde929c1e2fc45dd14
Inbeom Kim <dlsqja970210@naver.com>
2024-03-27 16:12:04 +0900

    ✨ 학습 관련 이미지 컨텐츠 컴포넌트 구현

---
+++ src/components/features/Learning/ImageContent/ImageContent.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+// Props 타입 정의
+// interface CurrentRoundProps {
+//   round: number;
+// }
+// const CurrentRound: React.FC<CurrentRoundProps> = ({ round }) => {
+
+const round = 1;
+
+const ImageContent = () => {
+  
+  return (
+    <div className="flex items-center text-lg bg-white bg-opacity-70 rounded p-4 text-center w-96 h-96">
+      <img 
+        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbmxKXWtkUK3IrPeJrE1rcHRpnYtBg7OsTrnJyihDYI41eTl_uW8RK_BWKdzWUuVaSagbBffI7FEKjZzeFGwxF6w7YO=w958-h910" 
+        alt="content" 
+      />
+    </div>
+  );
+};
+
+export default ImageContent;
--- src/pages/PersonalTrainningPage/LearningPage.tsx
+++ src/pages/PersonalTrainningPage/LearningPage.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import useStore from "../../store/learning";
 import VocabularyExplain from "../../components/features/Learning/VocabularyExplain/VocabularyExplain";
 import CurrentRound from "../../components/features/Learning/CurrentRound/CurrentRound";
+import ImageContent from "../../components/features/Learning/ImageContent/ImageContent";
 
 const Learning: React.FC = () => {
   const words = useStore((state) => state.words); // Zustand 스토어에서 words 배열을 가져옵니다.
@@ -19,6 +20,7 @@ const Learning: React.FC = () => {
     <div className="flex flex-col justify-center items-center h-screen gap-4">
       <CurrentRound />
       <VocabularyExplain />
+      <ImageContent />
     </div>
   );
 };
