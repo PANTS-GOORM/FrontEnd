2e107b1763e26d78388d5ac6ce101786b8fd6ede
Inbeom Kim <dlsqja970210@naver.com>
2024-03-27 15:14:20 +0900

    ✨ 학습에서 현재 라운드 컴포넌트 구현

---
+++ src/components/features/Learning/CurrentRound/CurrentRound.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+// Props 타입 정의
+// interface CurrentRoundProps {
+//   round: number;
+// }
+// const CurrentRound: React.FC<CurrentRoundProps> = ({ round }) => {
+
+const round = 1;
+
+const CurrentRound = () => {
+  
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex items-center text-lg bg-white bg-opacity-70 rounded p-4 text-center">
+        현재 {round} 라운드
+      </div>
+    </div>
+  );
+};
+
+export default CurrentRound;
--- src/pages/PersonalTrainningPage/LearningPage.tsx
+++ src/pages/PersonalTrainningPage/LearningPage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import React from "react";
 import useStore from "../../store/learning";
 import VocabularyExplain from "../../components/features/Learning/VocabularyExplain/VocabularyExplain";
+import CurrentRound from "../../components/features/Learning/CurrentRound/CurrentRound";
 
 const Learning: React.FC = () => {
   const words = useStore((state) => state.words); // Zustand 스토어에서 words 배열을 가져옵니다.
@@ -15,34 +16,8 @@ const Learning: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col items-center">
-        <div className="mb-4">
-          단어를 입력하고 엔터나 버튼을 누르면 단어가 추가되는 것 확인 가능
-        </div>
-        <form onSubmit={handleSubmit} className="flex flex-col items-center">
-          <input
-            type="text"
-            value={word}
-            onChange={(e) => setWord(e.target.value)}
-            placeholder="Add a new word"
-            className="w-full px-4 py-2 border rounded-md shadow-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-          <button
-            type="submit"
-            className="mt-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
-          >
-            Add Word
-          </button>
-        </form>
-        <h2>단어 목록</h2>
-        <br />
-        <ul>
-          {words.map((word, index) => (
-            <li key={index}>{word}</li> // words 배열의 각 단어를 리스트 아이템으로 변환하여 표시합니다.
-          ))}
-        </ul>
-      </div>
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <CurrentRound />
       <VocabularyExplain />
     </div>
   );
