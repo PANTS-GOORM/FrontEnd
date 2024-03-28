040100c4fee7ab1a48ba080c3cd5d941fc7e69bf
박민혁 <sis03090@naver.com>
2024-03-28 15:16:16 +0900

    💩 관리자 페이지 프로토타입용 임시 코드 작성

--- src/pages/AdminPage/AdminWordRegist.tsx
+++ src/pages/AdminPage/AdminWordRegist.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import useStore from "../../store/adminWord";
 
-function AdminWordRegist() {
+const AdminWordRegist: React.FC = () => {
+  const [word, setWord] = useState("");
+  const { words, addWord } = useStore((state) => state);
+
+  const handleChange = (e) => {
+    setWord(e.target.value);
+  };
+  const handleClickAddWord = () => {
+    addWord(word);
+    setWord("");
+    console.log(words);
+  };
   return (
-    <>
-      <h1>관리자 어휘 등록 페이지</h1>
-    </>
+    <div className="flex justify-center">
+      <div>
+        <h1>관리자 어휘 등록 페이지</h1>
+        <input
+          className="m-3"
+          type="text"
+          value={word}
+          onChange={handleChange}
+        ></input>
+        <button onClick={handleClickAddWord}>단어 등록</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default AdminWordRegist;
