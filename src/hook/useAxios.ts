29feeb7b8848a388b152d346ead36902b9e8e79d
Inbeom Kim <dlsqja970210@naver.com>
2024-04-19 16:06:34 +0900

    🐛 인증, 인가 url 변경 v0.4.1

--- src/hook/useAxios.ts
+++ src/hook/useAxios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 function useAxios() {
   const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: process.env.REACT_APP_API_URL,
+    // baseURL: process.env.REACT_APP_API_URL, 고민할 필요가 있을듯
   });
 
   return axiosInstance;
