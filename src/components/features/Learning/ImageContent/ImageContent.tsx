80826f3c9f1dbb1a542df470cf5e3600bda71b56
김민범 <beom0109@naver.com>
2024-04-23 10:23:03 +0900

    🔥 S3 url 주소 삭제
    s3 url 주소 삭제

--- src/components/features/Learning/ImageContent/ImageContent.tsx
+++ src/components/features/Learning/ImageContent/ImageContent.tsx
@@ -5,9 +5,6 @@ const ImageContent = () => {
   const contentURL = learningStore(
     (state) => state.contents[state.round - 1]?.contentURL || "기본 이미지 URL"
   );
-  console.log(contentURL);
-  //https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbmxKXWtkUK3IrPeJrE1rcHRpnYtBg7OsTrnJyihDYI41eTl_uW8RK_BWKdzWUuVaSagbBffI7FEKjZzeFGwxF6w7YO=w958-h910
-
   return (
     <div className="flex items-center text-lg bg-white bg-opacity-70 rounded p-4 text-center w-96 h-96">
       <img src={contentURL} alt="content" />
